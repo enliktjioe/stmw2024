2c3af5cae2efb2c971946de1d9967aeee9c33aac
Enlik <enlik@playtech.com>
2025-02-14 00:36:06 +0100

    Finished Part B: Create a Lucene Index

--- 20250211_pa2/runPA2.sh
+++ 20250211_pa2/runPA2.sh
@@ -4,6 +4,7 @@
 echo -e "Enlik - Programming Assignment 02"
 
 # Part A: Create a Spatial Index in MySQL
+echo "Part A: Create a Spatial Index in MySQL"
 echo "Dropping existing table..."
 mysql < dropSpatialIndex.sql
 echo "Tables dropped successfully."
@@ -13,3 +14,15 @@ echo "Creating new table..."
 mysql < createSpatialIndex.sql
 echo "Tables created successfully."
 printf "\n"
+
+# Part B: Create a Lucene Index
+echo "Part B: Create a Lucene Index"
+# Compile and run Indexer.java to create Lucene index
+echo "Compile java file..."
+javac Indexer.java
+echo "Java file compiled successfully."
+printf "\n"
+
+# Run the Java program Indexer
+echo "Running Indexer.java"
+java Indexer
